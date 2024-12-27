329f36cfd18ad292584ca75be43f7012df012446
Jaweria-B <bjaweria509@gmail.com>
2024-12-27 17:48:02 +0500

    added nodemon to project

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}));
 
 // app.use("/index.html", express.static(__dirname + 'index.html'));
+app.use(express.static(__dirname));
 
 var messages = [
     {
@@ -22,7 +23,8 @@ var server = app.listen(3000, () =>{
     console.log('Server is running on port: 3000', server.address().port)
 })
 app.get('/', function (req, res) {
-    res.render('/index');
+    // res.render('/index');
+    res.sendFile(__dirname + '/index.html');
 })
 app.get('/messages', function (req, res) {
     res.send(messages);
